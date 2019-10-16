19e027cd3dbf026ab8d322e2953994ad05637b35
Erik Lindblom <elindblom@mediciventures.com>
2019-10-16 13:46:46 -0600

    Check for empty string in ENDPOINT variable

--- ngrok-wait.sh
+++ ngrok-wait.sh
@@ -5,16 +5,15 @@ NGROK_NAME=${NGROK_NAME:-ngrok}
 echo "ngrok end point [$NGROK_NAME]"
 
 ENDPOINT=null
-while [ $ENDPOINT = "null" ]
+while [ -z "$ENDPOINT" ] || [ "$ENDPOINT" = "null" ]
 do
     echo "Fetching end point from ngrok service"
     ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
 
-    if [ $ENDPOINT = "null" ]; then
+    if [ -z "$ENDPOINT" ] || [ "$ENDPOINT" = "null" ]; then
         echo "ngrok not ready, sleeping 5 seconds...."
         sleep 5
     fi
-
 done
 
 echo "fetched end point [$ENDPOINT]"
